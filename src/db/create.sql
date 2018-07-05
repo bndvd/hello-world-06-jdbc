2449b6e34bed188a97560df26b2aa039304c4973
BD <bd002@nym.hush.com>
2018-07-05 00:23:38 -0500

    initial version

---
+++ src/db/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `ride_tracker`.`ride` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `duration` INT NOT NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `ride_tracker`.`ride` 
+ADD COLUMN `ride_date` VARCHAR(45) NULL AFTER `duration`;
---
+++ src/main/java/com/pluralsight/repository/RideRepository.java
@@ -0,0 +1,16 @@
+package com.pluralsight.repository;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideRepository {
+
+	Ride createRide(Ride ride);
+	List<Ride> getRides();
+	Ride getRide(Integer id);
+	Ride updateRide(Ride ride);
+	void updateRides(List<Object[]> pairs);
+	void delete(Integer id);
+
+}
